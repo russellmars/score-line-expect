ab950104536d77d4686805cf808bd7475d3a624d
russellmars <russellmars@sina.com>
2018-07-10 09:18:37 +0800

    更新2018年数据

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="hello">
-    <yd-navbar title="二本学校分数期望值"></yd-navbar>
-    <p>
+  <div class="hello" style="padding-bottom: 10px;">
+    <yd-navbar title="湖南二本学校分数期望值"></yd-navbar>
+    <div>
       <h3>标准分数线</h3>
       <br/>
       <yd-cell-group
         :title="standardScoreLine.year + '年'"
-        :key="standardScoreLine"
+        :key="standardScoreLine.year"
         v-for="standardScoreLine in standardScoreLineList">
         <yd-cell-item>
           <span slot="left">一本线：</span>
@@ -17,26 +17,26 @@
           <yd-input slot="right" type="number" required max="20" v-model.number="standardScoreLine.secondScoreLine" placeholder="请输入分数"></yd-input>
         </yd-cell-item>
       </yd-cell-group>
-    </p>
-    <p>
+    </div>
+    <div>
       <template v-if="universityList.length > 0">
         <br/>
         <h3>学校投档线填写</h3>
         <br/>
       </template>
       <yd-cell-group
-        :key="university"
+        :key="university.key"
         v-for="university in universityList">
         <yd-cell-item>
           <span slot="left">学校名称：</span>
           <yd-input slot="right" required max="20" v-model="university.name" placeholder="请输入学校名称"></yd-input>
         </yd-cell-item>
-        <yd-cell-item v-for="scoreLine in university.scoreLines" :key="scoreLine">
+        <yd-cell-item v-for="scoreLine in university.scoreLines" :key="scoreLine.year">
           <span slot="left">{{ scoreLine.year }}年投档线：</span>
           <yd-input slot="right" required max="20" v-model="scoreLine.score" placeholder="请输入分数"></yd-input>
         </yd-cell-item>
         <yd-cell-item>
-          <span slot="left">2017年期望投档线：</span>
+          <span slot="left">{{currentYear}}年期望投档线：</span>
           <div slot="right">
             <span>{{ university.expectScore }}</span>
             <yd-button type="primary" @click.native="countExpectScore(university)">计算</yd-button>
@@ -46,7 +46,7 @@
       <yd-button-group>
         <yd-button type="primary" size="large" @click.native="addUniversity">新增学校</yd-button>
       </yd-button-group>
-    </p>
+    </div>
   </div>
 </template>
 
@@ -54,20 +54,18 @@
   export default {
     data () {
       return {
+        currentYear: 2018,
         yearCountNum: 4,
         standardScoreLineList: [],
         universityList: []
       }
     },
     created () {
-      this.standardScoreLineList = Array.from({length: this.yearCountNum}).map((val, index) => {
-        return {
-          year: 2016 - index,
-          firstScoreLine: 0,
-          secondScoreLine: 0
-        }
-      })
       this.standardScoreLineList = [{
+        year: 2018,
+        firstScoreLine: 513,
+        secondScoreLine: 450
+      }, {
         year: 2017,
         firstScoreLine: 505,
         secondScoreLine: 424
@@ -83,21 +81,18 @@
         year: 2014,
         firstScoreLine: 522,
         secondScoreLine: 442
-      }, {
-        year: 2013,
-        firstScoreLine: 495,
-        secondScoreLine: 423
       }]
     },
     methods: {
       addUniversity () {
         this.universityList.push({
+          key: Math.random(),
           name: '',
           expectScore: 0,
           scoreLines: Array.from({length: this.yearCountNum}).map((val, index) => {
             return {
               score: 0,
-              year: 2016 - index
+              year: this.currentYear - index - 1
             }
           })
         })
